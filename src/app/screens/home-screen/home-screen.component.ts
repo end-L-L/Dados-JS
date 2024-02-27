97503da21d3942b2386edd39a0ecf33887e00110
Boset <boss-x86@protonmail.com>
2024-02-27 00:37:42 -0600

    game + record

--- src/app/screens/home-screen/home-screen.component.ts
+++ src/app/screens/home-screen/home-screen.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FacadeService } from 'src/app/services/facade.service';
+import { ApiService } from 'src/app/services/api.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,6 +12,9 @@ export class HomeScreenComponent implements OnInit{
   
   // Session Properties
   public token: string = "";
+  public id: any = "";
+  public dataRecord: any = {};
+  public record_user: any = {};
 
   // Game Properties
   currentScore: number = 0; // Almacena la puntuación actual, inicializada en 0.
@@ -26,6 +30,7 @@ export class HomeScreenComponent implements OnInit{
 
   constructor(
     private facadeService: FacadeService,
+    private apiService: ApiService,
     private router: Router
   ) { }
 
@@ -35,12 +40,18 @@ export class HomeScreenComponent implements OnInit{
     this.token = this.facadeService.getSessionToken();
     console.log("Token: ", this.token);
     
+    // ID Initialization
+    this.id = this.facadeService.getUserId();
+
     if(this.token == ""){
       this.router.navigate([""]);
-    }  
+    }
 
     // Game Initialization
     this.updateDifficulty();
+
+    // Record Initialization
+    this.obtenerRecord();
   }
 
   rollDice(): void {
@@ -64,12 +75,46 @@ export class HomeScreenComponent implements OnInit{
     this.currentScore = 0;
   }
 
+  flag = 1;
+
   checkWin(score: number, scoreNeeded: number): void {
-    if (score === scoreNeeded) {
-      alert('¡Ganaste!');
-    } else {
-      alert('Intenta de nuevo.');
+  if (score === scoreNeeded) {
+    alert('¡Ganaste!');
+    this.flag = 1;
+  } else {
+    alert('Intenta de nuevo.');
+    this.flag = 0;
+  }
+    console.log("Result: ", this.flag);
+    console.log("Level: ", this.difficultySettings[this.difficulty].dice);
+    console.log("id", this.id);
+  
+    this.dataRecord = {
+      result: this.flag,
+      level: this.difficultySettings[this.difficulty].dice,
+      id: this.id
     }
+    this.apiService.actualizarRecord(this.dataRecord).subscribe({
+      next: (response) => {
+        console.log("Record Actualizado: ", response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+  
+  
+  public obtenerRecord(){
+    this.apiService.obtenerRecord().subscribe({
+      next: (response) => {
+        console.log("Record: ", response);
+        this.record_user = response;
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
   }
 
   logOut() {
